3d9e7d80c5ffe38823aad7c974d66df72806400b
Benjamin Maynor <BenjaminMaynor@gmail.com>
2021-08-16 12:30:52 -0500

    Panic recovery should now give the origin of the panic for debugging purposes.

--- utils.go
+++ utils.go
@@ -1,8 +1,10 @@
 package gojson
 
 import (
-	"errors"
+	"bytes"
 	"fmt"
+	"runtime/debug"
+	"strings"
 )
 
 // PanicRecovery returns a general use Panic Recovery function to capture panics
@@ -12,16 +14,36 @@ import (
 // usage: defer PanicRecovery(&err)()
 func PanicRecovery(err *error) {
 	if r := recover(); r != nil && err != nil {
+		origin := panicOrigin(debug.Stack())
+
 		// Create a new error and assign it to our pointer.
-		switch r.(type) {
+		switch r := r.(type) {
 		case error:
-			*err = r.(error)
+			*err = fmt.Errorf(`%w (%s)`, r, origin)
 		case string:
-			*err = errors.New(r.(string))
+			*err = fmt.Errorf(`%s (%s)`, r, origin)
 		default:
-			*err = fmt.Errorf("Panic. Context: %+v", r)
+			*err = fmt.Errorf("panic. context: %+v (%s)", r, origin)
 		}
 	}
+}
+
+// Find the origin of a panic so we can annotate our error.
+func panicOrigin(raw []byte) string {
+	stack := bytes.Split(raw, []byte{'\n'})
+
+	// Lines:
+	// 0 is goroutineID
+	// 1-2 are debug.Stack itself.
+	// 3-4 are this file's PanicRecovery function.
+	// 5-6 are runtime.Panic
+	// 7-8 are the origination, where 8 has the info we need.
+
+	if len(stack) < 9 {
+		return "unknown panic"
+	}
+
+	return strings.TrimSpace(string(stack[8]))
 }
 
 // Truncate returns a truncated byte slice if the length of the original slice is greater
